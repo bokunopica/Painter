efdfdeccda3d5dae760185f44285e27c903815a3
qq <qianq9720@163.com>
2023-06-20 17:33:06 +0800

    train.sh update

--- SegGPT/SegGPT_train/train_seggpt.sh
+++ SegGPT/SegGPT_train/train_seggpt.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 python /home/qianq/mycodes/Painter/SegGPT/SegGPT_train/main_train.py \
     --batch_size 1 \
-    --accum_iter 1  \
+    --accum_iter 16  \
     --ckpt_path /home/qianq/mycodes/Painter/SegGPT/SegGPT_inference/pretrained_seggpt/seggpt_vit_large.pth \
-    --num_mask_patches 392 \
-    --max_mask_patches_per_block 196 \
+    --num_mask_patches 784 \
+    --max_mask_patches_per_block 392 \
     --epochs 15 \
     --warmup_epochs 1 \
     --lr 1e-3 \
     --clip_grad 3 \
     --layer_decay 0.8 \
     --drop_path 0.1 \
-    --input_size 448 224 \
+    --input_size 896 448 \
     --save_freq 1 \
     --data_path /run/media/breastCancer/processed \
     --json_path  \
